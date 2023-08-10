b705495a66b34dec86d8060042ce4d8b81535d47
droge1 <emelobearinze@gmail.com>
2023-08-10 08:19:47 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main -this is my main function
+* Return: 0 is always 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
