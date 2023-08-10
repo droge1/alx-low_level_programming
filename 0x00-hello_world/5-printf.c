5d5c362184c7a81bfdb0fb68556fea33b910a45f
droge1 <emelobearinze@gmail.com>
2023-08-10 08:27:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - this is my main
+*Return: 0 is always 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
