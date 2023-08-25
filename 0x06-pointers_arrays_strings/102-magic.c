807d0cddbc6552873c43afe81fc522becbbd2dd6
droge1 <emelobearinze@gmail.com>
2023-08-24 17:24:31 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,7 +3,7 @@
 /**
  * main - This is the enry point of the magic program
  *
- * Return: Return 0 on sucess,
+ * Return: Return 0 on sucess.
  */
 int main(void)
 {
@@ -13,14 +13,6 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
